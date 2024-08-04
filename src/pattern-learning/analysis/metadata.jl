37fd6a894ecbb1ae24e05b85807921eddd3b406c
Alexander Herbrich <aherbrich32@gmail.com>
2024-08-04 18:00:44 +0200

    now possible to compute maximum possible accuracy for provided data (i.e. a model which would always should the most played move for a given board)

--- src/pattern-learning/analysis/metadata.jl
+++ src/pattern-learning/analysis/metadata.jl
@@ -54,11 +54,14 @@ mutable struct TestMetadata
     nr_of_games::Int
     const predictions::Vector{Prediction}
     global_start_time::Float64
+    # for each board, remember at which which move was played how often at which ply
+    hashtable::Dict{UInt64, Dict{UInt64, Vector{Int}}}
 end
 
 function TestMetadata(test_file::AbstractString)
     nr_of_games = count_lines_in_files(test_file)
-    return TestMetadata(0, nr_of_games, Vector{Prediction}(), time())
+
+    return TestMetadata(0, nr_of_games, Vector{Prediction}(), time(), Dict{UInt64, Dict{UInt64, Vector{Int}}}())
 end
 
 function Base.show(io::IO, metadata::TestMetadata)
@@ -70,7 +73,7 @@ function Base.show(io::IO, metadata::TestMetadata)
     end
 end
 
-function plot_metadata(metadata::TestMetadata)
+function plot_metadata(metadata::TestMetadata; plot_max_accuracy=false)
     # plot accuracy per ply
     correct_model_per_ply = zeros(Int, 100)
     correct_random_per_ply = zeros(Int, 100)
@@ -85,8 +88,38 @@ function plot_metadata(metadata::TestMetadata)
     accuracy_model_per_ply = correct_model_per_ply ./ total_per_ply
     accuracy_random_per_ply = correct_random_per_ply ./ total_per_ply
 
-    plt = plot(1:100, accuracy_model_per_ply, label="Model", xlabel="Ply", ylabel="Accuracy", title="Accuracy per ply")
+    plt = plot(1:100, accuracy_model_per_ply, label="Model", xlabel="Ply", ylabel="Accuracy", title="Accuracy per ply", legend=:topright)
     plot!(1:100, accuracy_random_per_ply, label="Random")
+
+    if plot_max_accuracy
+        correct_max_model_per_ply = zeros(Float64, 100)
+        seen_at_each_ply = zeros(Int, 100)
+
+        # calculate the maximum possible accuracy for the model
+        # for this we saved for each board, which move was played how often at which ply
+        for (board_hash, move_dict) in metadata.hashtable
+            # for a board, we have to find the move that was played most often
+            # over all plies (since we need to make a consistent decision on each ply)
+            # => else we overestimate the accuracy (since we would always choose the most played move for each ply)
+            # and not the most played move over all plies
+            best_move = reduce((x, y) -> sum(move_dict[x]) > sum(move_dict[y]) ? x : y, keys(move_dict))
+            
+            # now we have to calculate the accuracy per ply if we always played the most played move (over all plies)
+            for (move_hash, counts) in move_dict
+                for (i, count) in enumerate(counts)
+                    if move_hash == best_move
+                        correct_max_model_per_ply[min(i, 100)] += count
+                    end
+                    seen_at_each_ply[min(i, 100)] += count
+                end
+            end
+        end
+
+
+        accuracy_max_model_per_ply = correct_max_model_per_ply ./ seen_at_each_ply
+
+        plot!(1:100, accuracy_max_model_per_ply, label="Max Model")
+    end
 
     display(plt)
 end
--- src/pattern-learning/test.jl
+++ src/pattern-learning/test.jl
@@ -1,4 +1,4 @@
-function test_on_game(game_str::T, model::ValueTable, metadata::TestMetadata) where T <: AbstractString
+function test_on_game(game_str::T, model::ValueTable, metadata::TestMetadata; compute_max_accuracy=false) where T <: AbstractString
     # SET BOARD INTO INITIAL STATE
     board = Board()
     set_by_fen!(board, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
@@ -16,17 +16,36 @@ function test_on_game(game_str::T, model::ValueTable, metadata::TestMetadata) wh
 
         prediction = predict_on(model, board, legals)
         push!(metadata.predictions, prediction)
+        
+        if compute_max_accuracy
+            board_hash = board.history[board.ply].hash
+            move_hash = move_to_hash(move)
+
+            if !haskey(metadata.hashtable, board_hash)
+                metadata.hashtable[board_hash] = Dict{UInt64, Vector{Int}}()
+            end
+
+            if !haskey(metadata.hashtable[board_hash], move_hash)
+                metadata.hashtable[board_hash][move_hash] = zeros(Int, 100)
+            end
+
+            metadata.hashtable[board_hash][move_hash][min(board.ply, 100)] += 1
+        end
 
         do_move!(board, move)
     end
 
 end
 
-function test_model(filename_model::T, filename_games) where T <: AbstractString
+function test_model(filename_model::T, filename_games; compute_max_accuracy=false) where T <: AbstractString
     model = load_model(filename_model)
 
     # HELPER VARIABLES
     metadata = TestMetadata(filename_games)
+
+    if compute_max_accuracy
+        @warn "compute_max_accuracy is set to true!\nThis should only be used if really necessary, since it is very memory intensive!"
+    end
 
     # TEST MODEL
     games = open(filename_games, "r")
@@ -35,11 +54,11 @@ function test_model(filename_model::T, filename_games) where T <: AbstractString
         game_str = strip(readline(games))
 
         # TEST ON GAME
-        test_on_game(game_str, model, metadata)
+        test_on_game(game_str, model, metadata, compute_max_accuracy=compute_max_accuracy)
         print(metadata)
     end
 
-    plot_metadata(metadata)
+    plot_metadata(metadata, plot_max_accuracy=compute_max_accuracy)
 
     close(games)
 
